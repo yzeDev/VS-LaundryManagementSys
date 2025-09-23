b5f8a9795e1c1f546d6e195c0d60523fe2d970fc
yzeDev <yzekeilv@gmail.com>
2025-09-23 19:00:16 +0800

    v1.5.2

--- MainForm.Designer.vb
+++ MainForm.Designer.vb
@@ -45,7 +45,7 @@ Partial Class MainForm
         Panel2.Location = New Point(3, 3)
         Panel2.Name = "Panel2"
         TableLayoutPanel1.SetRowSpan(Panel2, 5)
-        Panel2.Size = New Size(124, 555)
+        Panel2.Size = New Size(94, 555)
         Panel2.TabIndex = 7
         ' 
         ' TableLayoutPanel2
@@ -66,75 +66,80 @@ Partial Class MainForm
         TableLayoutPanel2.RowStyles.Add(New RowStyle(SizeType.Percent, 20F))
         TableLayoutPanel2.RowStyles.Add(New RowStyle(SizeType.Percent, 20F))
         TableLayoutPanel2.RowStyles.Add(New RowStyle(SizeType.Percent, 20F))
-        TableLayoutPanel2.Size = New Size(124, 555)
+        TableLayoutPanel2.Size = New Size(94, 555)
         TableLayoutPanel2.TabIndex = 0
         ' 
         ' btnDashboard
         ' 
+        btnDashboard.Anchor = AnchorStyles.None
+        btnDashboard.AutoSizeMode = AutoSizeMode.GrowAndShrink
         btnDashboard.BackgroundImage = My.Resources.Resources._1
         btnDashboard.BackgroundImageLayout = ImageLayout.Zoom
-        btnDashboard.Dock = DockStyle.Fill
         btnDashboard.FlatAppearance.BorderSize = 0
         btnDashboard.FlatStyle = FlatStyle.Flat
-        btnDashboard.Location = New Point(3, 3)
+        btnDashboard.Location = New Point(12, 20)
         btnDashboard.Name = "btnDashboard"
-        btnDashboard.Size = New Size(118, 105)
+        btnDashboard.Size = New Size(70, 70)
         btnDashboard.TabIndex = 5
         btnDashboard.TabStop = False
         btnDashboard.UseVisualStyleBackColor = True
         ' 
         ' btnOptions
         ' 
+        btnOptions.Anchor = AnchorStyles.None
+        btnOptions.AutoSizeMode = AutoSizeMode.GrowAndShrink
         btnOptions.BackgroundImage = My.Resources.Resources._5
         btnOptions.BackgroundImageLayout = ImageLayout.Zoom
-        btnOptions.Dock = DockStyle.Fill
         btnOptions.FlatAppearance.BorderSize = 0
         btnOptions.FlatStyle = FlatStyle.Flat
-        btnOptions.Location = New Point(3, 447)
+        btnOptions.Location = New Point(12, 464)
         btnOptions.Name = "btnOptions"
-        btnOptions.Size = New Size(118, 105)
+        btnOptions.Size = New Size(70, 70)
         btnOptions.TabIndex = 4
         btnOptions.TabStop = False
         btnOptions.UseVisualStyleBackColor = True
         ' 
         ' btnStats
         ' 
+        btnStats.Anchor = AnchorStyles.None
+        btnStats.AutoSizeMode = AutoSizeMode.GrowAndShrink
         btnStats.BackgroundImage = My.Resources.Resources._4
         btnStats.BackgroundImageLayout = ImageLayout.Zoom
-        btnStats.Dock = DockStyle.Fill
         btnStats.FlatAppearance.BorderSize = 0
         btnStats.FlatStyle = FlatStyle.Flat
-        btnStats.Location = New Point(3, 336)
+        btnStats.Location = New Point(12, 353)
         btnStats.Name = "btnStats"
-        btnStats.Size = New Size(118, 105)
+        btnStats.Size = New Size(70, 70)
         btnStats.TabIndex = 3
         btnStats.TabStop = False
         btnStats.UseVisualStyleBackColor = True
         ' 
         ' btnTransactions
         ' 
+        btnTransactions.Anchor = AnchorStyles.None
+        btnTransactions.AutoSizeMode = AutoSizeMode.GrowAndShrink
         btnTransactions.BackgroundImage = My.Resources.Resources._3
         btnTransactions.BackgroundImageLayout = ImageLayout.Zoom
-        btnTransactions.Dock = DockStyle.Fill
         btnTransactions.FlatAppearance.BorderSize = 0
         btnTransactions.FlatStyle = FlatStyle.Flat
-        btnTransactions.Location = New Point(3, 225)
+        btnTransactions.Location = New Point(12, 242)
         btnTransactions.Name = "btnTransactions"
-        btnTransactions.Size = New Size(118, 105)
+        btnTransactions.Size = New Size(70, 70)
         btnTransactions.TabIndex = 2
         btnTransactions.TabStop = False
         btnTransactions.UseVisualStyleBackColor = True
         ' 
         ' btnMachines
         ' 
+        btnMachines.Anchor = AnchorStyles.None
+        btnMachines.AutoSizeMode = AutoSizeMode.GrowAndShrink
         btnMachines.BackgroundImage = My.Resources.Resources._2
         btnMachines.BackgroundImageLayout = ImageLayout.Zoom
-        btnMachines.Dock = DockStyle.Fill
         btnMachines.FlatAppearance.BorderSize = 0
         btnMachines.FlatStyle = FlatStyle.Flat
-        btnMachines.Location = New Point(3, 114)
+        btnMachines.Location = New Point(12, 131)
         btnMachines.Name = "btnMachines"
-        btnMachines.Size = New Size(118, 105)
+        btnMachines.Size = New Size(70, 70)
         btnMachines.TabIndex = 1
         btnMachines.TabStop = False
         btnMachines.UseVisualStyleBackColor = True
@@ -143,16 +148,16 @@ Partial Class MainForm
         ' 
         MainPanel.AutoScroll = True
         MainPanel.Dock = DockStyle.Fill
-        MainPanel.Location = New Point(133, 3)
+        MainPanel.Location = New Point(103, 3)
         MainPanel.Name = "MainPanel"
         TableLayoutPanel1.SetRowSpan(MainPanel, 5)
-        MainPanel.Size = New Size(948, 555)
+        MainPanel.Size = New Size(978, 555)
         MainPanel.TabIndex = 5
         ' 
         ' TableLayoutPanel1
         ' 
         TableLayoutPanel1.ColumnCount = 2
-        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 130F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 100F))
         TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle())
         TableLayoutPanel1.Controls.Add(MainPanel, 1, 0)
         TableLayoutPanel1.Controls.Add(Panel2, 0, 0)
@@ -160,12 +165,11 @@ Partial Class MainForm
         TableLayoutPanel1.Location = New Point(0, 0)
         TableLayoutPanel1.Name = "TableLayoutPanel1"
         TableLayoutPanel1.RowCount = 5
-        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 15.0657682F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 15.0657663F))
         TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 21.2335587F))
         TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 21.2335587F))
         TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 21.2335587F))
         TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 21.2335587F))
-        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 20F))
         TableLayoutPanel1.Size = New Size(1084, 561)
         TableLayoutPanel1.TabIndex = 0
         ' 
