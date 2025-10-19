293a916649ef16794944e862538543f60ef65837
Ysa <Ysa@LAPTOP-N8T8MV47>
2025-10-19 22:28:15 +0800

    aasasa

--- InvoiceForm.vb
+++ InvoiceForm.vb
@@ -3,5 +3,6 @@
     Private Sub InvoiceForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Initialization code here
     End Sub
-    ' Additional event handlers and methods here
+
+
 End Class
