22b86dd926ec10fcad1fcc1470b0bc18734a7f37
Ysa <Ysa@LAPTOP-N8T8MV47>
2025-10-20 02:09:39 +0800

    transac option

--- TransactionOption.vb
+++ TransactionOption.vb
@@ -1,8 +1,9 @@
 ﻿Imports System.Transactions
 
 Public Class TransactionOption
-    Private Sub btnCancelOrder_Click(sender As Object, e As EventArgs) Handles btnCancelOrder.Click
+
+
+    Private Sub btnCancelOrder_Click_1(sender As Object, e As EventArgs) Handles btnCancelOrder.Click
         Close()
     End Sub
-
 End Class
