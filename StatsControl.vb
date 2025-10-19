97a1290c75ee6f1be3690ca1df0171ae60ce1584
Myasora <owenmiranda042016@gmail.com>
2025-10-19 23:30:15 +0800

    10/19/25 v4

--- StatsControl.vb
+++ StatsControl.vb
@@ -276,7 +276,11 @@ Public Class StatsControl
 
     End Sub
 
-    Private Sub Label15_Click_1(sender As Object, e As EventArgs) Handles Label15.Click
+    Private Sub Label15_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
     End Sub
 End Class
