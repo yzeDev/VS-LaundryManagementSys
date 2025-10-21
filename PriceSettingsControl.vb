bd66f7b7101dd0b702de7c19a2996ac69306cebc
Myasora <owenmiranda042016@gmail.com>
2025-10-21 22:40:26 +0800

    v1

--- PriceSettingsControl.vb
+++ PriceSettingsControl.vb
@@ -22,4 +22,88 @@
     Private Sub Label17_Click(sender As Object, e As EventArgs)
 
     End Sub
+
+    Private Sub selfservice_newWash_TextChanged(sender As Object, e As EventArgs) Handles selfservice_newWash.TextChanged
+
+    End Sub
+
+    Private Sub selfservice_currentWash_Click(sender As Object, e As EventArgs) Handles selfservice_currentWash.Click
+
+    End Sub
+
+    Private Sub selfservice_newDry_TextChanged(sender As Object, e As EventArgs) Handles selfservice_newDry.TextChanged
+
+    End Sub
+
+    Private Sub selfservice_currentDry_Click(sender As Object, e As EventArgs) Handles selfservice_currentDry.Click
+
+    End Sub
+
+    Private Sub selfservice_newWashDry_TextChanged(sender As Object, e As EventArgs) Handles selfservice_newWashDry.TextChanged
+
+    End Sub
+
+    Private Sub selfservice_currentWashDry_Click(sender As Object, e As EventArgs) Handles selfservice_currentWashDry.Click
+
+    End Sub
+
+    Private Sub basicservice_newWash_TextChanged(sender As Object, e As EventArgs) Handles basicservice_newWash.TextChanged
+
+    End Sub
+
+    Private Sub basicservice_currentWash_Click(sender As Object, e As EventArgs) Handles basicservice_currentWash.Click
+
+    End Sub
+
+    Private Sub basicservice_newDry_TextChanged(sender As Object, e As EventArgs) Handles basicservice_newDry.TextChanged
+
+    End Sub
+
+    Private Sub basicservice_currentDry_Click(sender As Object, e As EventArgs) Handles basicservice_currentDry.Click
+
+    End Sub
+
+    Private Sub basicservice_newWashDry_TextChanged(sender As Object, e As EventArgs) Handles basicservice_newWashDry.TextChanged
+
+    End Sub
+
+    Private Sub basicservice_currentWashDry_Click(sender As Object, e As EventArgs) Handles basicservice_currentWashDry.Click
+
+    End Sub
+
+    Private Sub fullservice_new2_TextChanged(sender As Object, e As EventArgs) Handles fullservice_new2.TextChanged
+
+    End Sub
+
+    Private Sub fullservice_currentWash_Click(sender As Object, e As EventArgs) Handles fullservice_currentWash.Click
+
+    End Sub
+
+    Private Sub dryclean_newSmall_TextChanged(sender As Object, e As EventArgs) Handles dryclean_newSmall.TextChanged
+
+    End Sub
+
+    Private Sub dryclean_currentSmall_Click(sender As Object, e As EventArgs) Handles dryclean_currentSmall.Click
+
+    End Sub
+
+    Private Sub dryclean_newMedium_TextChanged(sender As Object, e As EventArgs) Handles dryclean_newMedium.TextChanged
+
+    End Sub
+
+    Private Sub dryclean_currentMedium_Click(sender As Object, e As EventArgs) Handles dryclean_currentMedium.Click
+
+    End Sub
+
+    Private Sub dryclean_newLarge_TextChanged(sender As Object, e As EventArgs) Handles dryclean_newLarge.TextChanged
+
+    End Sub
+
+    Private Sub dryclean_currentLarge_Click(sender As Object, e As EventArgs) Handles dryclean_currentLarge.Click
+
+    End Sub
+
+    Private Sub saveBtn_Click(sender As Object, e As EventArgs) Handles saveBtn.Click
+
+    End Sub
 End Class
