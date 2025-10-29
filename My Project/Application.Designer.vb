1c74e4341f67a6ffe1d01ac7e983915fc1eb23fb
yzeDev <yzekeilv@gmail.com>
2025-10-30 04:35:35 +0800

    fixes

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.LaundryManagementSys.PaymentDetails
+            Me.MainForm = Global.LaundryManagementSys.MainForm
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
